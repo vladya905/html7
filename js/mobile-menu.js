91c0f004062862fbb50f0aa218642c5da39c0977
Lubajkin Vladislav <125284590+vladya905@users.noreply.github.com>
2024-09-20 20:01:09 +0300

    commit1

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", togglemenu);
+  refs.closeMenuBtn.addEventListener("click", togglemenu);
+
+  function togglemenu() {
+    refs.menu.classList.toggle("is-hidden");
+    document.body.classList.toggle('no-scroll');
+  }
+})();
